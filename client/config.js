2d399b93bf4ca1d59d4663380c2127f1f99e4d42
Max <tongweizj@gmail.com>
2025-12-17 20:55:27 -0500

    update

--- client/config.js
+++ client/config.js
@@ -1,4 +1,9 @@
 const config = {
+    API_BASE_URL:"http://localhost:3001",
+    IMAGE_BASE_URL: 'http://localhost:5174/',
+}
+
+const configdev = {
     API_BASE_URL:"http://192.168.2.150:8310",
     IMAGE_BASE_URL: 'http://192.168.2.150:8300',
 }
